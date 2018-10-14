6b6ddd34acbde5e50812650eee189cc3e184b0e2
Asha <43291441+Asha2805@users.noreply.github.com>
2018-10-14 06:45:59 -0400

    Update and rename README.md to spaceasha.java

---
+++ spaceasha.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class spaceasha
+{
+public static void main(String[] args)
+{
+String se,sw;
+Scanner w=new Scanner(System.in)
+for(int i=0;i<se.length();i++)
+{
+sw=se.replaceAll(" "," ");
+}
+System.out.println(sw);
+}
+}
